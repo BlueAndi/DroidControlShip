0ed8ccd4a594c5fa0785af694c9d6e942222f042
BlueAndi <web@blue-andi.de>
2023-05-09 23:49:40 +0200

    Initial preparation

---
+++ lib/ArduinoNative/Arduino.cpp
@@ -0,0 +1,123 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  Arduino native
+ * @author Andreas Merkle <web@blue-andi.de>
+ */
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include <Arduino.h>
+
+/******************************************************************************
+ * Compiler Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and classes
+ *****************************************************************************/
+
+/******************************************************************************
+ * Prototypes
+ *****************************************************************************/
+
+extern void setup();
+extern void loop();
+
+/******************************************************************************
+ * Local Variables
+ *****************************************************************************/
+
+/******************************************************************************
+ * Public Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * Protected Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * Private Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * External Functions
+ *****************************************************************************/
+
+#ifdef UNIT_TEST
+
+extern int main(int argc, char** argv)
+{
+    setup(); /* Prepare test */
+    loop();  /* Run test once */
+
+    return 0;
+}
+
+#else
+
+extern int main(int argc, char** argv)
+{
+    int status = 0;
+
+    setup();
+
+    while (1)
+    {
+        loop();
+    }
+
+    return status;
+}
+
+#endif
+
+extern unsigned long millis()
+{
+    clock_t now = clock();
+
+    return (now * 1000UL) / CLOCKS_PER_SEC;
+}
+
+extern void delay(unsigned long ms)
+{
+    unsigned long timestamp = millis();
+
+    while ((millis() - timestamp) < ms)
+    {
+        ;
+    }
+}
+
+/******************************************************************************
+ * Local Functions
+ *****************************************************************************/
