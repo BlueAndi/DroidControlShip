03a9e9a204ac3d5bc41b339faea74d22bf1a8c02
greyes <gabryelrdiaz@gmail.com>
2023-06-03 16:22:52 +0200

    Added Files with dummy values or no implementation

---
+++ lib/HALTarget/src/Device.h
@@ -0,0 +1,108 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  Device realization
+ * @author Gabryel Reyes <gabryelrdiaz@gmail.com>
+ *
+ * @addtogroup HALTarget
+ *
+ * @{
+ */
+
+#ifndef DEVICE_H
+#define DEVICE_H
+
+/******************************************************************************
+ * Compile Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include "IDevice.h"
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and Classes
+ *****************************************************************************/
+
+/** This class provides access to the simulation device. */
+class Device : public IDevice
+{
+public:
+    /**
+     * Constructs the device adapter.
+     */
+    Device() : IDevice()
+    {
+    }
+
+    /**
+     * Destroys the device adapter.
+     */
+    virtual ~Device()
+    {
+    }
+
+    /**
+     * Initialize device driver.
+     *
+     * @return If successfully initialized, returns true. Otherwise, false.
+     */
+    bool init() final;
+
+    /**
+     * Process communication with the device.
+     *
+     * @return If communication is successful, returns true. Otherwise, false.
+     */
+    bool process() final;
+
+    /**
+     * Get comunication Stream.
+     *
+     * @return Device data Stream.
+     */
+    Stream& getStream() final;
+
+    /**
+     * Reset the device.
+     */
+    void reset() final;
+
+private:
+};
+
+/******************************************************************************
+ * Functions
+ *****************************************************************************/
+
+#endif /* DEVICE_H */
+/** @} */
