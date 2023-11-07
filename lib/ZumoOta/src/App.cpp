101800c033ad312dca8f58789a49beae8ac1b08d
Decareme <ngandeca@yahoo.fr>
2023-11-07 16:37:01 +0100

    Update Credentials in App.cpp

--- lib/ZumoOta/src/App.cpp
+++ lib/ZumoOta/src/App.cpp
@@ -66,8 +66,8 @@ Upload upload;
 static const uint32_t SERIAL_BAUDRATE = 115200U;
 
 /*defines the WiFi Credentials*/
-const char* ssid = "Djokoh";
-const char* password = "djokep26nov2015";
+const char* ssid = "your_ssid";
+const char* password = "WiFipassword";
 /******************************************************************************
  * Public Methods
  *****************************************************************************/
