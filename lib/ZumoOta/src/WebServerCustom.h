7e38d89e86dd137fa2e8ec5c90c07d7c62ab2329
greyes <gabryelrdiaz@gmail.com>
2023-11-30 23:03:42 +0100

    Formatted all relevant ZumoOta files

--- lib/ZumoOta/src/WebServerCustom.h
+++ lib/ZumoOta/src/WebServerCustom.h
@@ -31,7 +31,7 @@
 class WebServerCustom
 {
 private:
-AsyncWebServer server{80};
+    AsyncWebServer server{80};
 
 public:
     /**
@@ -45,48 +45,47 @@ public:
     ~WebServerCustom();
 
     /**
-        * @brief Initializes the web server.
-        * 
-        * This function is used to initialize the web server and set it up for handling requests.
-    */
+     * @brief Initializes the web server.
+     *
+     * This function is used to initialize the web server and set it up for handling requests.
+     */
     void start();
 
     /**
-        * 
-        *This function is used to initialize the web server and set it up for handling requests. 
-        * 
-    */
+     *
+     *This function is used to initialize the web server and set it up for handling requests.
+     *
+     */
     void init();
 
     /**
-        * 
-        * This function sets up a handler for processing HTTP POST requests to /upload.
-        * 
-    */
+     *
+     * This function sets up a handler for processing HTTP POST requests to /upload.
+     *
+     */
     void handleUploadRequest();
 
     /**
-    * @brief Makes the size of files human-readable.
-    * 
-    * This function converts the file size in bytes to a human-readable format.
-    * 
-    * @param bytes The size of the file in bytes
-    * @return A string representing the human-readable file size
-    */
+     * @brief Makes the size of files human-readable.
+     *
+     * This function converts the file size in bytes to a human-readable format.
+     *
+     * @param bytes The size of the file in bytes
+     * @return A string representing the human-readable file size
+     */
     String humanReadableSize(const size_t bytes);
 
     /**
-    * @brief Lists files stored on LittleFS in either HTML or plaintext format.
-    * 
-    * This function generates a list of files stored on LittleFS and returns it as a string.
-    * The output format can be either HTML or plaintext, depending on the value of the `ishtml` parameter.
-    * 
-    * @param ishtml Flag indicating whether to generate HTML-formatted output (true) or plaintext (false)
-    * @return A string containing the list of files in the specified format
-    */
+     * @brief Lists files stored on LittleFS in either HTML or plaintext format.
+     *
+     * This function generates a list of files stored on LittleFS and returns it as a string.
+     * The output format can be either HTML or plaintext, depending on the value of the `ishtml` parameter.
+     *
+     * @param ishtml Flag indicating whether to generate HTML-formatted output (true) or plaintext (false)
+     * @return A string containing the list of files in the specified format
+     */
     String listFiles(bool ishtml);
 };
 
-#endif/* WEBSERVER_H */
+#endif /* WEBSERVER_H */
 /** @} */
-
