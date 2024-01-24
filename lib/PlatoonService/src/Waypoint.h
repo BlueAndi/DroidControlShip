49a0741113fd36bf34fe10d5667708cfc4e153e4
gabryelreyes <gabryel.reyes@newtec.de>
2024-01-24 14:49:53 +0100

    Moved serialization of waypoint to a class member function

--- lib/PlatoonService/src/Waypoint.h
+++ lib/PlatoonService/src/Waypoint.h
@@ -44,6 +44,7 @@
  *****************************************************************************/
 
 #include <stdint.h>
+#include <WString.h>
 
 /******************************************************************************
  * Macros
@@ -57,8 +58,9 @@
  * Waypoint structure definition.
  * Defines the position of a waypoint in the map and the speed at which is to be reached.
  */
-typedef struct _Waypoint
+class Waypoint
 {
+public:
     int32_t xPos;        /**< X position [mm]. */
     int32_t yPos;        /**< Y position [mm]. */
     int32_t orientation; /**< Orientation [mrad]. */
@@ -69,11 +71,53 @@ typedef struct _Waypoint
     /**
      * Default constructor.
      */
-    _Waypoint() : xPos(0), yPos(0), orientation(0), left(0), right(0), center(0)
+    Waypoint() : Waypoint(0, 0, 0, 0, 0, 0)
     {
     }
 
-} __attribute__((packed)) Waypoint;
+    /**
+     * Constructor
+     *
+     * @param[in] xPos          X position [mm].
+     * @param[in] yPos          Y position [mm].
+     * @param[in] orientation   Orientation [mrad].
+     * @param[in] left          Left motor speed [steps/s].
+     * @param[in] right         Right motor speed [steps/s].
+     * @param[in] center        Center speed [steps/s].
+     */
+    Waypoint(int32_t xPos, int32_t yPos, int32_t orientation, int16_t left, int16_t right, int16_t center) :
+        xPos(xPos),
+        yPos(yPos),
+        orientation(orientation),
+        left(left),
+        right(right),
+        center(center)
+    {
+    }
+
+    /**
+     * Deserialize a waypoint.
+     * The waypoint is created on the heap and must be deleted by the caller.
+     *
+     * @param[in]   serializedWaypoint  Serialized waypoint.
+     *
+     * @return Pointer to a waypoint object. In case of an error, it returns nullptr.
+     */
+    static Waypoint* deserialize(const String& serializedWaypoint);
+
+    /**
+     * Serialize the waypoint.
+     *
+     * @return Serialized waypoint. Returns an empty string in case of an error.
+     */
+    String serialize() const;
+
+    /**
+     * Print waypoint data to the serial console.
+     * Uses the LOG_DEBUG macro, so it can be deactivated.
+     */
+    void debugPrint() const;
+};
 
 /******************************************************************************
  * Functions
