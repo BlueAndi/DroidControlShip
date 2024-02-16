b048212db2ceecb4bb1a057c51498929b7948a2e
gabryelreyes <gabryel.reyes@newtec.de>
2024-02-16 12:41:59 +0100

    Removed shifting the angle to the positive range of atan2

--- lib/PlatoonService/src/PlatoonUtils.cpp
+++ lib/PlatoonService/src/PlatoonUtils.cpp
@@ -86,8 +86,7 @@ bool PlatoonUtils::calculateHeading(const Waypoint& targetWaypoint, const Waypoi
     if ((0 != deltaX) || (0 != deltaY))
     {
         /* Calculate target heading. */
-        float angle = atan2(deltaY, deltaX) * 1000.0F;         /* Angle in mrad. */
-        angle += (FP_2PI() + 0.5F);                            /* Shift angle by 360 degree. */
+        float angle  = atan2(deltaY, deltaX) * 1000.0F;        /* Angle in mrad. */
         heading      = static_cast<int32_t>(angle) % FP_2PI(); /* Fixed point heading. */
         isSuccessful = true;
     }
