3da2baa8d4af395ac66b2834403a37860612ebf5
Gabryel Reyes <gabryel.reyes@newtec.de>
2024-03-14 09:29:24 +0100

    Fixed missing debug status send

--- lib/ConvoyFollower/src/App.cpp
+++ lib/ConvoyFollower/src/App.cpp
@@ -373,6 +373,14 @@ void App::processPeriodicTasks()
         m_commandTimer.restart();
     }
 
+    if (true == m_sendWaypointTimer.isTimeout())
+    {
+        if (false == m_v2vCommManager.sendStatus(m_latestVehicleData))
+        {
+            LOG_WARNING("Status could not be sent.");
+        }
+    }
+
     if ((true == m_sendWaypointTimer.isTimeout()) && (true == m_mqttClient.isConnected()) &&
         (true == DrivingState::getInstance().isActive()) &&
         (50U <= PlatoonUtils::calculateAbsoluteDistance(m_lastWaypointSent, m_latestVehicleData)))
