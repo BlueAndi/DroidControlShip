22ccc4d4c252d197c9fef6f9726e9a188ea07122
jkerpe <juliane-98@web.de>
2024-03-18 08:03:15 +0100

    Corrected Casting from Float to Integer

--- lib/SensorFusion/src/App.cpp
+++ lib/SensorFusion/src/App.cpp
@@ -229,9 +229,9 @@ void App::publishSensorFusionPosition()
 
     /* Write newest Sensor Fusion Data in JSON String */
     IKalmanFilter::PositionData currentPosition = m_sensorFusion.getLatestPosition();
-    payloadJson["positionX"]                    = static_cast<int32_t>(currentPosition.positionX);
-    payloadJson["positionY"]                    = static_cast<int32_t>(currentPosition.positionY);
-    payloadJson["angle"]                        = static_cast<int32_t>(currentPosition.angle);
+    payloadJson["positionX"]                    = static_cast<int32_t>(currentPosition.positionX + 0.5F);
+    payloadJson["positionY"]                    = static_cast<int32_t>(currentPosition.positionY + 0.5F);
+    payloadJson["angle"]                        = static_cast<int32_t>(currentPosition.angle + 0.5F);
     (void)serializeJson(payloadJson, payloadArray);
     String payloadStr(payloadArray);
     bool   wasPublishingSucessful = m_mqttClient.publish(TOPIC_NAME_POSITION, false, payloadStr);
