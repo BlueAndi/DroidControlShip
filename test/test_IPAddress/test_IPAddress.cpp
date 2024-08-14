a4f3ccddbd8861c16cb62d76aa355ac6647f4eb4
Norbert Schulz <github@schulznorbert.de>
2024-08-14 17:19:33 +0200

    ArduinoNative: Add IPAddress class

---
+++ test/test_IPAddress/test_IPAddress.cpp
@@ -0,0 +1,150 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 - 2024 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  Test (some) IPAddress functions.
+ * @author Norbert Schulz <schulz.norbert@gmail.com>
+ */
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include <unity.h>
+#include <IPAddress.cpp>
+
+/******************************************************************************
+ * Compiler Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and classes
+ *****************************************************************************/
+
+/******************************************************************************
+ * Prototypes
+ *****************************************************************************/
+
+static void testIPAddrConstruction(void);
+static void testIPAddrString(void);
+
+/******************************************************************************
+ * Local Variables
+ *****************************************************************************/
+
+/******************************************************************************
+ * Public Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * Protected Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * Private Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * External Functions
+ *****************************************************************************/
+
+/**
+ * Main entry point
+ *
+ * @param[in] argc  Number of command line arguments
+ * @param[in] argv  Command line arguments
+ *
+ * @returns Test failure count
+ */
+extern int main(int argc, char** argv)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(testIPAddrConstruction);
+    RUN_TEST(testIPAddrString);
+
+    return UNITY_END();
+}
+
+/**
+ * Setup a test. This function will be called before every test by unity.
+ */
+extern void setUp(void)
+{
+    /* Not used. */
+}
+
+/**
+ * Clean up test. This function will be called after every test by unity.
+ */
+extern void tearDown(void)
+{
+    /* Not used. */
+}
+
+/******************************************************************************
+ * Local Functions
+ *****************************************************************************/
+
+/**
+ * Test IPAddr construction.
+ */
+static void testIPAddrConstruction(void)
+{
+    IPAddress ipDefault;
+    TEST_ASSERT_EQUAL_UINT32(0x00000000, ipDefault.raw());
+
+    IPAddress ipInt32(0xBADCAFFE);
+    TEST_ASSERT_EQUAL_UINT32(0xBADCAFFE, ipInt32.raw());
+
+    IPAddress ip;
+    ip.fromString(String("192.168.1.42"));
+    TEST_ASSERT_EQUAL_UINT32(0xC0A8012A, ip.raw());
+
+    IPAddress other(ip);
+    TEST_ASSERT_EQUAL_UINT32(0xC0A8012A, other.raw());
+
+    other = ipInt32;
+    TEST_ASSERT_EQUAL_UINT32(0xBADCAFFE, other.raw());
+}
+
+static void testIPAddrString(void)
+{
+    IPAddress ip;
+
+    String str("192.168.1.42");
+    TEST_ASSERT_TRUE(ip.fromString(str));
+    TEST_ASSERT_EQUAL_UINT32(0xC0A8012A, ip.raw());
+
+    String prn(ip.toString());
+    TEST_ASSERT_EQUAL_STRING(str.c_str(), prn.c_str());
+
+    String crab("The quick brown fox jumps over the lazy dog");
+    TEST_ASSERT_FALSE(ip.fromString(crab));
+}
