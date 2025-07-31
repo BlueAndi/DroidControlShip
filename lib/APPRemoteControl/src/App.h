f5ab71f5f5ca270c3a6b84f68d69e782e4514563
BlueAndi <web@blue-andi.de>
2025-07-31 23:32:01 +0200

    Fixed doxygen parameter name.

--- lib/APPRemoteControl/src/App.h
+++ lib/APPRemoteControl/src/App.h
@@ -159,8 +159,8 @@ private:
      * Setup the MQTT connection.
      * 
      * @param[in] clientId      The MQTT client id.
-     * @param[in] brokderAddr   The address of the MQTT broker.
-     * @param[in] brokderPort   The port of the MQTT broker.
+     * @param[in] brokerAddr    The address of the MQTT broker.
+     * @param[in] brokerPort    The port of the MQTT broker.
      * 
      * @return true if successful, otherwise false.
      */
