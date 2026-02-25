e07c089df6458bc0451aac41fd52fcc65d7b2a6b
Stefan Vogel <stefan.vogel@newtec.de>
2026-02-25 09:09:48 +0100

    refactor: turn clang-format on after block

--- lib/APPTurtle/src/App.cpp
+++ lib/APPTurtle/src/App.cpp
@@ -298,7 +298,7 @@ void App::handleTurtle()
         const int32_t MILLI_CONVERSION_FACTOR = 1000;
         int32_t       linearSpeed             = m_turtleSpeedSetpoint.linear.x * MILLI_CONVERSION_FACTOR;  // Linear speed in mm/s
         int32_t       angularSpeed            = m_turtleSpeedSetpoint.angular.z * MILLI_CONVERSION_FACTOR; // Angular speed in mrad/s
-        // clang-format off
+        // clang-format on
 
         payload.linearCenter = linearSpeed;
         payload.angular      = angularSpeed;
